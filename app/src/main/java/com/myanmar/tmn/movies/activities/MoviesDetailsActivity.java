05ff9bc802a89b0dac7be763e0dee6bbac0a8f76
Thet Myat Noe <thetmyatnoe871994@gmail.com>
2017-12-16 15:28:22 +0630

    16evening commit

--- app/src/main/java/com/myanmar/tmn/movies/activities/MoviesDetailsActivity.java
+++ app/src/main/java/com/myanmar/tmn/movies/activities/MoviesDetailsActivity.java
@@ -35,7 +35,7 @@ public class MoviesDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.items_movies_details);
+        setContentView(R.layout.activity_movies_details);
         ButterKnife.bind(this,this);
 
         genreAdapter = new GenreAdapter();
